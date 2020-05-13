ca60184dd2c18b3ad7bc1b9c44754ce27cd62e5d
thekamakaji <43650594+thekamakaji@users.noreply.github.com>
2020-05-13 13:53:23 -0400

    Added properties to stock and stock interval. Implemented experimental graphing in test.py

--- data/AlphaVantageP930.py
+++ data/AlphaVantageP930.py
@@ -27,32 +27,132 @@ class stock():
     #return pp.pformat(self.__dict__)
     #return str(self.__dict__)
     dict = collections.OrderedDict()
-    dict['ticker'] = self.ticker
-    dict['interval'] = self.interval
+    dict['ticker'] = self._ticker
+    dict['interval'] = self._interval
+    dict['length'] = self._length
     dict['interval_data']= "DATA LIST"
     
     return pp.pformat(dict)
     
     
   def __init__(self):
-    self.ticker = None
-    self.interval = None
-    self.interval_data = []
+    self._ticker = None
+    self._interval = None
+    self._interval_data = []
+    self._length = 0
   
   def Intraday(self, data):
-    self.ticker = data['Meta Data']['2. Symbol']
-    self.interval = data['Meta Data']['4. Interval']
-    _intervals_key = 'Time Series (' + self.interval + ')'
+    self._ticker = data['Meta Data']['2. Symbol']
+    self._interval = data['Meta Data']['4. Interval']
+    _intervals_key = 'Time Series (' + self._interval + ')'
     
     for key in list(data[_intervals_key].keys()):
       interval_data = data[_intervals_key][key]
-      self.interval_data = [stock_interval(key, interval_data)] + self.interval_data
+      self._interval_data = [stock_interval(key, interval_data)] + self._interval_data
+      
+    self._length = len(self._interval_data)
 
   def _dict(self):
     interval_list = []
     for interval in self.intervals:
       interval_list += [interval.dict()]
     return interval_list
+    
+  def get_ticker(self):
+    return self._ticker
+  ticker = property(get_ticker)
+  
+  def get_interval(self):
+    return self._interval
+  interval = property(get_interval)
+  
+  def interval(self, index):
+    return self._interval_data[index]
+  
+  def get_length(self):
+    return self._length
+  length = property(get_length)
+  
+  def get_interval_data(self):
+    return self._interval_data
+  interval_data = property(get_interval_data)
+  
+  def get_keys(self):
+    key_list = []
+    for interval in self._interval_data:
+      key_list += [interval.key]
+    return key_list
+  keys = property(get_keys)
+  
+  def get_opens(self):
+    open_list = []
+    for interval in self._interval_data:
+      open_list += [interval.open]
+    return open_list
+  opens = property(get_opens)
+
+  def get_highs(self):
+    high_list = []
+    for interval in self._interval_data:
+      high_list += [interval.high]
+    return high_list
+  highs = property(get_highs)
+  
+  def get_highs2(self):
+    high_list = []
+    for high in self.get_highs():
+      high_list += [high, high]
+    return high_list
+  highs2 = property(get_highs2)
+  
+  def get_lows(self):
+    low_list = []
+    for interval in self._interval_data:
+      low_list += [interval.low]
+    return low_list
+  lows = property(get_lows)
+  
+  def get_lows2(self):
+    low_list = []
+    for low in self.get_lows():
+      low_list += [low, low]
+    return low_list
+  lows2 = property(get_lows2)
+  
+  def get_closes(self):
+    close_list = []
+    for interval in self._interval_data:
+      close_list += [interval.close]
+    return close_list
+  closes = property(get_closes)
+  
+  def get_volumes(self):
+    volume_list = []
+    for interval in self._interval_data:
+      volume_list += [interval.volume]
+    return volume_list
+  volumes = property(get_volumes)
+  
+  def get_opens_closes(self):
+    oc_list = []
+    for interval in self._interval_data:
+      oc_list += [interval.open, interval.close]
+    return oc_list
+  opens_closes = property(get_opens_closes)
+  
+  def get_index_single(self):
+    index_list = []
+    for i in range(self._length):
+      index_list += [i]
+    return index_list
+  index_single = property(get_index_single)
+  
+  def get_index_double(self):
+    index_list = []
+    for i in range(self._length):
+      index_list += [i, i+1]
+    return index_list
+  index_double = property(get_index_double)
 
 class stock_interval():
   
@@ -64,7 +164,6 @@ class stock_interval():
     dict['low'] = self._low,
     dict['close'] = self._close,
     dict['volume'] = self._volume
-    
     return dict
   
   
@@ -78,7 +177,8 @@ class stock_interval():
     
   def get_key(self):
     return self._key
-    
+  key = property(get_key)
+  
   def get_open(self):
     return float(self._open)
   open = property(get_open)
--- data/test.py
+++ data/test.py
@@ -1,8 +1,19 @@
-import pprint
-pp = pprint.PrettyPrinter(indent = 4)
-print = pp.pprint
+import matplotlib.pyplot as plt
+import numpy as np
+from AlphaVantageP930 import *
 
-import requests
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=GOOG&apikey=0GGIM0L0U42EVGVN'
-r = requests.get(url)
-print(r.json)
+plt.plot(a.index_double, a.lows2, label = 'low')
+plt.plot(a.index_double, a.highs2, label = 'high')
+plt.plot(a.index_double, a.opens_closes, label = 'open/close')
+plt.plot(a.index_single, a.opens, label = 'open')
+plt.plot([x+1 for x in a.index_single], a.closes, label = 'close')
+
+plt.xlim(0,a.length)
+
+plt.legend()
+
+plt.xticks(a.index_single, a.keys, rotation = 90)
+
+plt.grid()
+
+plt.show()
